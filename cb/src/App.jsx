ea1b5c2f47c47c3abb88f5e95c213342cbfdd017
kumarmanojgit <krmanoj72099@gmail.com>
2025-01-01 23:15:18 +0530

    complete setup

---
+++ cb/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Bot from "./componet/Bot";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>CHat Ai</h1>
+        <Bot />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ cb/src/componet/Bot.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Bot = () => {
+  return <div>bot</div>;
+};
+
+export default Bot;
